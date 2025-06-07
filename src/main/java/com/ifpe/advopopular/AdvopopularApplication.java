54a759758eb3034ab1bfd244ce491d1711c2372e
Laércio Rodrigues <laercio.rodrigs@gmail.com>
2025-06-07 11:08:37 -0300

    inicializa projeto

---
+++ src/main/java/com/ifpe/advopopular/AdvopopularApplication.java
@@ -0,0 +1,13 @@
+package com.ifpe.advopopular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvopopularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvopopularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifpe/advopopular/AdvopopularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpe.advopopular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvopopularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
